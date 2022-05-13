97ab787fb6b9e6e167b1f72b71186dd917253f30
VasylFrantsishkevych <vasul.fr@gmail.com>
2022-05-13 09:58:53 +0300

    lesson 1

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {IUser} from "../../modules";
+import {UserService} from "../../services";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.css']
+})
+export class UsersComponent implements OnInit {
+
+  users: IUser[];
+
+  constructor(private userService: UserService) { }
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(value => this.users = value);
+  }
+
+}
